de17ea43a93c4216ec1a2f75a73632004e41f77b
184rishabh <1842001rishabsingh@gmail.com>
2021-12-18 17:27:52 +0530

    i

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
-const cors=require('cors');
 const path=require('path');
 
 const authroute=require('./routes/auth')
